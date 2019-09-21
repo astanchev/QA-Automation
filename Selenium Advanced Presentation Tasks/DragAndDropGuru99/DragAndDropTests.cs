d10bf6b543f2e29e70682e9f587455823087046d
Атанас Станчев <mr_Stanchev@hotmail.com>
2019-09-22 01:50:52 +0300

    Added problems prom the presentation

---
+++ Selenium Advanced Presentation Tasks/DragAndDropGuru99/DragAndDropTests.cs
@@ -0,0 +1,106 @@
+﻿namespace DragAndDropGuru99
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using System.Threading;
+    using NUnit.Framework;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Interactions;
+    using OpenQA.Selenium.Support.UI;
+
+    [TestFixture]
+    public class DragAndDropTests
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+        private Actions builder;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl(@"http://demo.guru99.com/test/drag_drop.html");
+
+            builder = new Actions(driver);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void TestDragLoanDisplaysErrorMessage()
+        {
+            var loanBox =
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath(@"//*[@id='credit4']/a")));
+
+
+            builder.MoveToElement(loanBox).ClickAndHold().MoveByOffset(1, 1).Perform();
+
+            var errorMessage = driver.FindElement(By.XPath(@"//*[@id='e1']"));
+
+            Assert.AreEqual("Please select another block", errorMessage.Text);
+
+        }
+
+
+        [Test]
+        public void TestDragSalesToCreditSideAccount()
+        {
+            var salesBox =
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath(@"//*[@id='credit1']/a")));
+
+            var placeToDropInCreditSideAccount = driver.FindElement(By.XPath(@"//*[@id='loan']/li"));
+
+
+            builder.DragAndDrop(salesBox, placeToDropInCreditSideAccount).Perform();
+
+            var placeSalesIsShownInCreditSideAccount = driver.FindElement(By.XPath(@"//*[@id='loan']/li"));
+
+            Assert.AreEqual("SALES", placeSalesIsShownInCreditSideAccount.Text);
+
+        }
+
+
+        [Test]
+        public void TestPerfectButtonIsDisplayed()
+        {
+            var salesBox = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath(@"//*[@id='credit1']/a")));
+            var placeToDropInCreditSideAccount =  wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath(@"//*[@id='loan']/li")));
+            builder.DragAndDrop(salesBox, placeToDropInCreditSideAccount).Build().Perform();
+
+
+            builder = new Actions(driver);
+            var bankBox =  wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath(@"//*[@id='credit2']/a")));
+            var placeToDropInDebitSideAccount = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath(@"//*[@id='bank']/li")));
+            builder.DragAndDrop(bankBox, placeToDropInDebitSideAccount).Build().Perform();
+            
+
+            builder = new Actions(driver);
+            var fiveThousandBoxFirst = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath(@"//*[@id='fourth'][1]/a")));
+            var placeToDropInDebitSideAmount =  wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath(@"//*[@id='amt7']/li")));
+            builder.DragAndDrop(fiveThousandBoxFirst, placeToDropInDebitSideAmount).Build().Perform();
+            
+
+            builder = new Actions(driver);
+            var fiveThousandBoxSecond =  wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath(@"//*[@id='fourth'][2]/a")));
+            var placeToDropInCreditSideAmount =  wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath(@"//*[@id='amt8']/li")));
+            builder.DragAndDrop(fiveThousandBoxSecond, placeToDropInCreditSideAmount).Build().Perform();
+
+
+            var perfectButton = driver.FindElement(By.XPath(@"//*[@id='equal']/a"));
+
+            Assert.IsTrue(perfectButton.Displayed);
+        }
+
+    }
+}
---
+++ Selenium Advanced Presentation Tasks/PresentationTasks/Tests.cs
@@ -0,0 +1,104 @@
+﻿namespace PresentationTasks
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using NUnit.Framework;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Support.UI;
+
+    [TestFixture]
+    public class Tests
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            driver.Manage().Window.Maximize();
+
+            driver.Navigate().GoToUrl(@"http://toolsqa.com/automation-practice-switch-windows/");
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void TestBrowserWindow()
+        {
+            var newBrWindowButton =
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id(@"button1")));
+            newBrWindowButton.Click();
+
+            //WindowHandles returns collection of window names
+            string newPopUpWindowName = driver.WindowHandles.Last();
+
+            var newPopUpWindow = driver.SwitchTo().Window(newPopUpWindowName); 
+
+            var logo =  wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementExists(By.XPath(@"//*[@id=""page""]/div[1]/div[2]/div[3]/a/img")));
+
+            var expectedSrc = @"https://www.toolsqa.com/wp-content/uploads/2014/08/Toolsqa.jpg";
+            var actualSrc = logo.GetProperty("src");
+
+            Assert.AreEqual(expectedSrc, actualSrc);
+
+            newPopUpWindow.Close();
+            
+            Assert.IsTrue(driver.WindowHandles.Count == 1);
+            
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
+        }
+
+        [Test]
+        public void TestNewBrowserTab()
+        {
+            var newNewBrowserTabButton =
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath(@"//*[@id='content']/div[1]/div[2]/div/div/div/div/p[5]/button")));
+            newNewBrowserTabButton.Click();
+
+            string newBrowserTabName = driver.WindowHandles.Last();
+
+            var newBrowserTab = driver.SwitchTo().Window(newBrowserTabName);
+
+            var actualNewBrowserTabTitle = driver.Title;
+            var expectedNewBrowserTabTitle = @"Free QA Automation Tools Tutorial for Beginners with Examples";
+
+            Assert.AreEqual(expectedNewBrowserTabTitle, actualNewBrowserTabTitle);
+
+            newBrowserTab.Close();
+
+            Assert.IsTrue(driver.WindowHandles.Count == 1);
+
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
+        }
+
+        [Test]
+        public void TestAlertBox()
+        {
+            var newAlertButton =
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id(@"alert")));
+            newAlertButton.Click();
+
+            var alert = driver.SwitchTo().Alert();
+
+            var message = "Knowledge increases by sharing but not by saving. Please share this website with your friends and in your organization.";
+           
+            Assert.AreEqual(message, alert.Text);
+            
+            alert.Dismiss();
+
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
+        }
+
+    }
+}
