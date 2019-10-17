a7ef2b70b2e3fd725f99229d7ae034fa0a6a17ec
Атанас Станчев <mr_Stanchev@hotmail.com>
2019-10-17 20:55:02 +0300

    Add Selenium Grid support

--- Homework-POM/Google_Search/GoogleSearchTests.cs
+++ Homework-POM/Google_Search/GoogleSearchTests.cs
@@ -4,11 +4,11 @@
 
     using OpenQA.Selenium;
     using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Remote;
 
     using Pages.GoogleSearchPage;
 
-    using System.IO;
-    using System.Reflection;
+    using System;
 
     [TestFixture]
     public class GoogleSearchTests
@@ -19,8 +19,14 @@
         [SetUp]
         public void SetUp()
         {
-            driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
-            driver.Manage().Window.Maximize();
+            ChromeOptions options = new ChromeOptions();
+
+            options.PlatformName = "windows";
+            options.BrowserVersion = "77.0";
+
+
+            driver = new RemoteWebDriver(new Uri("http://192.168.100.5:21928/wd/hub"), options.ToCapabilities(), TimeSpan.FromSeconds(10));
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(30);
 
             searchPage = new GoogleSearchPage(driver);
 
